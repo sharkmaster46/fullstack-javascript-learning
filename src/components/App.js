2df676aeb28a695ec559327dba56a9b83ef4323d
shark06 <freelancer.available@gmail.com>
2020-11-16 02:25:03 +0530

    Working with Data

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import ContestPreview from './ContestPreview';
 import Header from './Header';
+import axios from 'axios';
 
 class App extends React.Component {
     state = {
-        pageHeader: 'Naming Contests'
+        pageHeader: 'Naming Contests',
+        contests: []
     };
+    componentDidMount() {
+        axios.get('/api/contests')
+        .then( resp => {
+            this.setState({
+                contests: resp.data.contests
+            })
+        })
+        .catch(console.error)
+        
+    }
     render() {
         return (
             <div className="App">
                 <Header message={this.state.pageHeader} />
                 <div>
-                    ....
+                    {this.state.contests.map(contest => 
+                        <ContestPreview key={contest.id} {...contest} />
+                    )}
                 </div>
             </div>
         );
